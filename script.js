6ddd1bee5327af5dda7bd200332e5cf07b7aaba1
Kevin Hase <hasekevin21@googlemail.com>
2025-08-22 20:45:16 +0200

    add template

--- script.js
+++ script.js
@@ -18,8 +18,32 @@ function loadToLocalstorage() {
 
 function renderMenu() {
     let menu = document.getElementById("menu");
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
+    menu.innerHTML = "";
+    for (let i = 0; i < menus.length; i++) {
+        const myMenus = menus[i];
+        menu.innerHTML += `
+    <main>
+        <div>
+            <nav>
+                <a href="">Vorspeisen</a>
+                <a href="">Hauptspeisen</a>
+                <a href="">Desserts</a>
+                <a href="">Getränke</a>
+            </nav>
+        </div>
+        <h2>Vorspeisen</h2>
+            <p></p>
+            <p></p>
+        <h2>Hauptspeisen</h2>
+            <p></p>
+            <p></p>
+            <p></p>
+        <h2>Desserts</h2>
+            <p></p>
+            <p></p>
+        <h2>Getränke</h2>
+            <p></p>
+            <p></p>
+    </main>`
     }
 }
