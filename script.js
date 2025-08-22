d84386c8a8a669ef6d6a7d4a35614fc57f0d2da3
Kevin Hase <hasekevin21@googlemail.com>
2025-08-22 20:32:45 +0200

    extract JSON data into separate file

--- script.js
+++ script.js
@@ -1,115 +1,25 @@
-let menus = [
-    {
-        "name": "Olivenperlen mit Gin-Tonic-Nebel",
-        "ingredients": [
-            "Grüne Oliven",
-            "Gin",
-            "Tonic Water",
-            "Calciumlactat",
-            "Natriumalginat",
-            "Trockeneis"
-        ],
-        "price": 12.50
-    },
-    {
-        "name": "Lachs-Tatar mit Wasabi-Luft",
-        "ingredients": [
-            "Frischer Lachs",
-            "Wasabi",
-            "Sojasauce",
-            "Limettensaft",
-            "Lecithin",
-            "Sesam"
-        ],
-        "price": 14.90
-    },
-    {
-        "name": "Zerlegter Cheeseburger",
-        "ingredients": [
-            "Rinderfilet-Würfel",
-            "Tomaten-Gel",
-            "Senf-Sphäre",
-            "Cheddar-Crunch",
-            "Mini-Brioche",
-            "Essiggurkenstaub"
-        ],
-        "price": 18.90
-    },
-    {
-        "name": "Sous-Vide Rinderfilet mit Rotwein-Gel & Stickstoff-Kartoffelchips",
-        "ingredients": [
-            "Rinderfilet",
-            "Rotwein",
-            "Agar-Agar",
-            "Kartoffeln",
-            "Flüssiger Stickstoff",
-            "Rosmarin"
-        ],
-        "price": 26.50
-    },
-    {
-        "name": "Vegane Textur-Bombe",
-        "ingredients": [
-            "Gegrillte Jackfruit",
-            "Avocado-Mousse",
-            "Tomatenluft",
-            "Quinoa-Crunch",
-            "Basilikumöl"
-        ],
-        "price": 17.50
-    },
-    {
-        "name": "Schokoladen-Mousse mit Chili-Sphäre",
-        "ingredients": [
-            "Zartbitterschokolade",
-            "Sahne",
-            "Chili-Extrakt",
-            "Natriumalginat",
-            "Zucker"
-        ],
-        "price": 9.90
-    },
-    {
-        "name": "Zitronen-Caviar mit Lavendel-Schaum",
-        "ingredients": [
-            "Zitronensaft",
-            "Zucker",
-            "Calciumlactat",
-            "Lavendelblüten",
-            "Lecithin"
-        ],
-        "price": 8.50
-    },
-    {
-        "name": "Frozen Mojito-Bites",
-        "ingredients": [
-            "Rum",
-            "Limettensaft",
-            "Minze",
-            "Zucker",
-            "Flüssiger Stickstoff"
-        ],
-        "price": 7.90
-    },
-    {
-        "name": "Blauer Plasma-Cocktail",
-        "ingredients": [
-            "Blue Curaçao",
-            "Zitronensaft",
-            "Tonic Water",
-            "Essbares Leuchtpulver",
-            "LED-Strohhalm"
-        ],
-        "price": 10.50
-    }
-]
+
 
 function init() {
-    let menu = document.getElementById("menu");
+    renderMenu();
+    loadToLocalstorage();
     //menü anzeigen lassen
-    menu
+
     //wert zurückgeben
 }
+
+function loadToLocalstorage() {
+    if (!localStorage.getItem("menus")) {
+        localStorage.setItem("menus", JSON.stringify(books));
+    } else {
+        books = JSON.parse(localStorage.getItem("menus"));
+    }
+}
+
 function renderMenu() {
-    
+    let menu = document.getElementById("menu");
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
 }
