95626aa2155f7e414a9b2edf52beabcec88c0c28
Kevin Hase <hasekevin21@googlemail.com>
2025-08-22 16:00:57 +0200

    start bestellapp

---
+++ warenkorb/warenkorb.js
@@ -0,0 +1,87 @@
+let menus = ["Pizza Salami", "Spagetti Bolognese", "Bruschetta", "Döner"];
+let prices = [8.99, 6.99, 3.99, 2.50];
+let amounts = [1, 2, 3, 4];
+
+function getValueFromInput(id) {
+    // greife auf Input zu, lese Wert aus
+    let value = document.getElementById(id).value; // setze eine Variable, die Variable bekommt den Inhalt der Id, daraus hole ich mir den Value
+    //gebe ausgegeben Wert zurück
+    return value
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getValueFromInput(menus, prices) {
+//     const inputRef = document.getElementById();
+//     if (inputRef) {
+//         return inputRef.value;
+//     } else{
+//         return "";
+//     }
+// }
+
+// function getMenuFromInput() {
+//     const menuRef = document.getElementById("menu");
+//     menuRef.innerHTML = "";
+//     for (let indexMenu = 0; indexMenu < menus.length; indexMenu++) {
+//         const element = menus[indexMenu].trim;
+        
+//     }
+// }
+
+// function getPriceFromInput() {
+//     const priceRef = document.getElementById("price");
+//     priceRef.innerHTML = "";
+//     for (let indexPrice = 0; indexPrice < prices.length; indexPrice++) {
+//         const element = price[indexPrice];
+        
+//     }
+// }
+
+// function onAddMenu() {
+    
+// }
